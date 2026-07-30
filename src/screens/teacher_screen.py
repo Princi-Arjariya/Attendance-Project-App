119d56726a715025f699f95108acebafd67652df
Princi-Arjariya <purviarjariya21@gmail.com>
2026-07-30 15:45:37 +0000

    face_pipeline stablish in student_screen, voice_pipeline

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -7,15 +7,32 @@ from src.database.db import check_teacher_exists,create_teacher,teacher_login
 def teacher_screen():
     style_background_dashboard()
     style_base_layout()
-
-    if 'teacher_login_type' not in st.session_state or st.session_state.teacher_login_type=='login':
+    
+    if 'teacher_data' in st.session_state:
+        teacher_dashboard()
+    elif 'teacher_login_type' not in st.session_state or st.session_state.teacher_login_type=='login':
         teacher_screen_login()
     elif st.session_state.teacher_login_type=='register':
         teacher_screen_register()
 
 
-   
+def teacher_dashboard():
+    teacher_data = st.session_state.teacher_data
+    st.header(f"""Welcome {teacher_data['name']}""")
 
+def login_teacher(username,password):
+    if not username or not password:
+        return False
+
+    teacher  = teacher_login(username,password)
+    if teacher:
+        st.session_state.user_role = 'teacher'
+        st.session_state.teacher_data = teacher
+        st.session_state.is_logged_in = True
+        return True
+    return False
+
+    teacher = teacher_login
 def teacher_screen_login():
     c1,c2 = st.columns(2,vertical_alignment = "center",gap = "xxlarge")
     with c1:
@@ -40,7 +57,7 @@ def teacher_screen_login():
 
     with btnc1:
         if st.button('Login',icon = ':material/passkey:',shortcut= 'control+enter',width = 'stretch'):
-            if teacher_login(teacher_username,teacher_pass):
+            if login_teacher(teacher_username,teacher_pass):
                 st.toast("Welcome back!",icon = '🤝')
                 import time
                 time.sleep(1)
