b702ff669ec00a43b0182d95b3cdf3bd6f7fb05e
Princi-Arjariya <purviarjariya21@gmail.com>
2026-08-18 14:34:21 +0000

    fix ui

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -1,101 +1,199 @@
 import streamlit as st
 
+
+
 def style_background_home():
+
     st.markdown(
+
         """
+
         <style>
+
             .stApp{
-                
+
+               
+
                 background:#5865f2 !important;
+
             }
+
+
 
             .stApp div[data-testid="stColumn"]{
-                
+
+               
+
                 background-color:#E0E3FF !important;
+
                 padding:2.5rem !important;
+
                 border-radius:5rem !important;
-                
+
+               
+
                 }
-            
-        </style>
+
+           
+
+        </style
+
         """
+
     ,unsafe_allow_html = True)
+
+
 
 def style_background_dashboard():
+
     st.markdown(
+
         """
+
         <style>
+
             .stApp{
-                
+
+               
+
                 background:#E0E3FF !important;
+
             }
-            
-        </style>
+
+           
+
+        </style
+
         """
+
     ,unsafe_allow_html = True)
+
+
 
 def style_base_layout():
+
 # asdasd
+
     st.markdown(
+
         """
+
         <style>
+
          @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
+
          @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+
          /* Hide Top Bar of Streamlit */
-            
+
+           
+
             #MainMenu, header,footer{
+
                visibility:hidden;
+
             }
+
+
 
             .block_container{
-                padding-top:1.5rem !important;
-                }
-            h1 {
-               font-family:'Climate Crisis', sans-serif !important;
-               color: #2D3748 !important; /* White ki jagah dark color */
-                font-weight: 800 !important;
-               font-size:3.5rem !important;
-               line-height:1.2 !important;
-               margin-bottom:0.5rem !important;}
 
-            h2 {
+                padding-top:1.5rem !important;
+
+                }
+
+            h1 {
+
                font-family:'Climate Crisis', sans-serif !important;
-               font-size:2rem !important;
-               line-height:0.9 !important;
+
+               font-size:3.5rem !important;
+
+               line-height:1.1 !important;
+
                margin-bottom:0rem !important;}
 
+
+
+            h2 {
+
+               font-family:'Climate Crisis', sans-serif !important;
+
+               font-size:2rem !important;
+
+               line-height:0.9 !important;
+
+               margin-bottom:0rem !important;}
+
+
+
             h3,h4,p{
+
                font-family:'Outfit',sans-serif;
+
                }
-          
+
+         
+
             button{
+
                border-radius:1.5rem !important;
+
                background-color:#5865f2 !important;
+
                color: white !important;
+
                padding:10px,20px !important;
+
                border:None !important;
+
                transition: transform 0.25sec ease-in-out !important;
+
             }
+
             button[kind="secondary"]{
+
                border-radius:1.5rem !important;
+
                background-color:#EB459E !important;
+
                color: white !important;
+
                padding:10px,20px !important;
+
                border:None !important;
+
                transition: transform 0.25sec ease-in-out !important;
+
             }
+
+
 
             button[kind="tertiary"]{
+
                border-radius:1.5rem !important;
+
                background-color:black !important;
+
                color: white !important;
+
                padding:10px,20px !important;
+
                border:None !important;
+
                transition: transform 0.25sec ease-in-out !important;
+
             }
 
+
+
             button:hover{
+
                transform:scale(1.05)}
+
         </style
+
         """
-    ,unsafe_allow_html = True)
+
+    ,unsafe_allow_html = True) 
+
+
